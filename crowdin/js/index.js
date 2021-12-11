2376c26db2765206e85390fc104a370c8765e32a
Subbu Swaroop <subbuswaroop3795@gmail.com>
2021-12-11 17:47:39 +0530

    Change

--- crowdin/js/index.js
+++ crowdin/js/index.js
@@ -1,57 +1,45 @@
 const axios = require('axios').default;
 const AdmZip = require("adm-zip");
 const shell = require('shelljs');
-/** CONFIG ENV VARIABLES */
-const BASE_URL = "https://api.crowdin.com/api/v2/projects";
-// const PROJECT_ID = config.PROJECT_ID;
+import mergeTranslations from './merge_i18n_new';
 
-const CROWDIN_TOKEN = process.argv[3];
-const CROWDIN_PROJECT_ID = process.argv[2];
+const [, , CROWDIN_PROJECT_ID, CROWDIN_TOKEN] = process.argv;
+const BASE_URL = `https://api.crowdin.com/api/v2/projects/${CROWDIN_PROJECT_ID}/translations/builds`;
 
-const PROVIDER_CODE = ["es-ES", "ja", "es-MX", "no", "pt", "pt", "ru", "sv"];
-const APP_CODE = ["es", "ja-JP", "es-LA", "nb-NO", "pt-BR", "pt-PT", "ru-RU", "sv-SE"];
-const BASE_LANG = 'en';
+let checkBuilStatusInterval;
+const HEADERS = { 'Authorization': `Bearer ${CROWDIN_TOKEN}` };
 
-var checkBuilStatusInterval = null;
-const HEADERS = {
-  headers: {
-    'Authorization': `Bearer ${CROWDIN_TOKEN}`
-  }
-};
-
-function checkBuildStatus(buildId) {
+const checkBuildStatus = (buildId) => {
   console.log("Checking Build Status...");
-  const options = {
-    method: "GET",
-    ...HEADERS,
-    url: `${BASE_URL}/${CROWDIN_PROJECT_ID}/translations/builds/${buildId}`
-  };
 
-  return axios(options).then(response=> {
-    if(response.data && response.data.data) {
+  return axios.get({
+    headers: HEADERS,
+    url: `${BASE_URL}/${buildId}`
+  }).then(response=> {
+    if(response.data?.data) {
       const status = response.data.data.status;
       console.log("Status:: ", status);
-      if(status == "inProgress") {
+      if(status === "inProgress") {
         return status;
-      } else if(status == "finished") {
+      } else if(status === "finished") {
         clearInterval(checkBuilStatusInterval);
         getDownloadURL(buildId);
       }
       return status;
     }
+  }).catch(err=> {
+    console.log(err);
   })
 }
 
-function buildTranslations() {
+const buildTranslations = () => {
   console.log("Building Translations...");
-  const options = {
-    method: "POST",
-    ...HEADERS,
-    data: {},
-    url: `${BASE_URL}/${CROWDIN_PROJECT_ID}/translations/builds`
-  }
 
-  return axios(options).then(response=> {
+  return axios.post({
+    headers: HEADERS,
+    data: {},
+    url: BASE_URL
+  }).then(response=> {
     if(response.data && response.data.data) {
       let buildId = response.data.data.id;
       // The build process usually takes more than a minute due to large number of keys. So, we are only proceeding
@@ -61,37 +49,138 @@ function buildTranslations() {
   })
 }
 
-function getDownloadURL(buildId) {
+const getDownloadURL = (buildId) => {
   console.log("Fetching zip download url...");
   let zipURL = "";
-  const options = {
-    method: "GET",
-    ...HEADERS,
-    url: `${BASE_URL}/${CROWDIN_PROJECT_ID}/translations/builds/${buildId}/download`
-  };
 
-  axios(options).then(response=> {
-    if(response && response.data) {
+  axios.get({
+    headers: HEADERS,
+    url: `${BASE_URL}/${buildId}/download`
+  }).then(response => {
+    if(response?.data) {
       zipURL = response.data.data.url;
       downloadTranslations(zipURL);
     }
   }).catch(err=> {
     console.log(err);
   })
-  // `https://api.crowdin.com/api/v2/projects/${CROWDIN_PROJECT_ID}/languages/${languageId}/translations`
-
 }
 
-function downloadTranslations(zipURL) {
+const downloadTranslations = (zipURL) => {
   console.log("Downloading Translations...");
   axios.get(zipURL, { responseType: 'arraybuffer' }).then(res => {
     console.log('zip download status ', res.status);
     var zipFile = new AdmZip(res.data);
     zipFile.extractAllTo('./tmp');
     // shell.exec('git_home=$(git rev-parse --show-toplevel 2>&1)')
-    shell.exec(`node js/merge_i18n.js`);
+    mergeTranslations();
     shell.exec('rm -r tmp')
+  }).catch(err=> {
+    console.log(err);
   });
 }
 
 buildTranslations();
+
+
+
+// const axios = require('axios').default;
+// const AdmZip = require("adm-zip");
+// const shell = require('shelljs');
+// /** CONFIG ENV VARIABLES */
+// const BASE_URL = "https://api.crowdin.com/api/v2/projects";
+// // const PROJECT_ID = config.PROJECT_ID;
+
+// const CROWDIN_TOKEN = 'a755a1bd2e49947f4ce66a74e54c17b1d4b2d262b8c3b71e279660dfb38609d8264489e0c0356eb7';
+// const CROWDIN_PROJECT_ID = 489071;
+
+// const PROVIDER_CODE = ["es-ES", "ja", "es-MX", "no", "pt", "pt", "ru", "sv"];
+// const APP_CODE = ["es", "ja-JP", "es-LA", "nb-NO", "pt-BR", "pt-PT", "ru-RU", "sv-SE"];
+// const BASE_LANG = 'en';
+
+// var checkBuilStatusInterval = null;
+// const HEADERS = {
+//   headers: {
+//     'Authorization': `Bearer ${CROWDIN_TOKEN}`
+//   }
+// };
+
+// function checkBuildStatus(buildId) {
+//   console.log("Checking Build Status...");
+//   const options = {
+//     method: "GET",
+//     ...HEADERS,
+//     url: `${BASE_URL}/${CROWDIN_PROJECT_ID}/translations/builds/${buildId}`
+//   };
+
+//   return axios(options).then(response=> {
+//     if(response.data && response.data.data) {
+//       const status = response.data.data.status;
+//       console.log("Status:: ", status);
+//       if(status == "inProgress") {
+//         return status;
+//       } else if(status == "finished") {
+//         clearInterval(checkBuilStatusInterval);
+//         getDownloadURL(buildId);
+//       }
+//       return status;
+//     }
+//   })
+// }
+
+// function buildTranslations() {
+//   console.log("Building Translations...");
+//   const options = {
+//     method: "POST",
+//     ...HEADERS,
+//     data: {},
+//     url: `${BASE_URL}/${CROWDIN_PROJECT_ID}/translations/builds`
+//   }
+
+//   return axios(options).then(response=> {
+//     if(response.data && response.data.data) {
+//       let buildId = response.data.data.id;
+//       // The build process usually takes more than a minute due to large number of keys. So, we are only proceeding
+//       // to the next step if this build finishes. Until then, we check the status every 30 seconds
+//       checkBuilStatusInterval = setInterval(checkBuildStatus, 30000, buildId);
+//     }
+//   })
+// }
+
+// function getDownloadURL(buildId) {
+//   console.log("Fetching zip download url...");
+//   let zipURL = "";
+//   const options = {
+//     method: "GET",
+//     ...HEADERS,
+//     url: `${BASE_URL}/${CROWDIN_PROJECT_ID}/translations/builds/${buildId}/download`
+//   };
+
+//   axios(options).then(response=> {
+//     if(response && response.data) {
+//       zipURL = response.data.data.url;
+//       downloadTranslations(zipURL);
+//     }
+//   }).catch(err=> {
+//     console.log(err);
+//   })
+//   // `https://api.crowdin.com/api/v2/projects/${CROWDIN_PROJECT_ID}/languages/${languageId}/translations`
+
+// }
+
+// function downloadTranslations(zipURL) {
+//   console.log("Downloading Translations...");
+//   axios.get(zipURL, { responseType: 'arraybuffer' }).then(res => {
+//     console.log('zip download status ', res.status);
+//     var zipFile = new AdmZip(res.data);
+//     zipFile.extractAllTo('./tmp');
+//     // shell.exec('git_home=$(git rev-parse --show-toplevel 2>&1)')
+//     shell.exec(`node js/merge_i18n.js`);
+//     shell.exec('rm -r tmp')
+//   });
+// }
+
+// buildTranslations();
+
+
+
---
+++ crowdin/js/merge_i18n_new.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+
+// constants
+const mergeFolderPath = `${process.cwd()}/tmp`;
+const baseFolderPath = `/home/runner/work/crowdin-sync-repo/crowdin-sync-repo/locales-json`;
+
+const ignorableFolders = ['en', '.DS_Store'];
+
+const ignorableFiles = [
+  '602198.yml',
+  'Auto_Triage_keys.json',
+  'Raven',
+  'Robo_Assist_keys.json',
+  'SR-454243.yaml',
+  'SR-475094',
+  'Touchstone',
+  'agentAssist.json',
+  'freshdesk-analytics.properties',
+  // misc
+  '.DS_Store'
+];
+
+const codeMappings = {
+  'es-MX': 'es-LA',
+  'es-ES': 'es',
+  'ja': 'ja-JP',
+  'no': 'nb-NO',
+  'ru': 'ru-RU',
+  'lv': 'lv-LV'
+};
+
+const pluralizeKeys = ['zero', 'two', 'few', 'many'];
+
+// pluralize
+const pluralize = (keys) => {
+  let pluralized = {};
+
+  for (let key in keys) {
+    if (key.endsWith('.other')) {
+      let extractedKeyArray = key.split('.');
+      extractedKeyArray.pop();
+      let extractedKey = extractedKeyArray.join('.');
+
+      let value = keys[key];
+
+      pluralizeKeys.forEach((key) => {
+        pluralized[`${extractedKey}.${key}`] = value;
+      });
+    }
+  }
+
+  // merge
+  return Object.assign({}, keys, pluralized);
+};
+
+const mergeTranslations = () => {
+  // proceed
+  console.log(`🏁 Starting merge with: ${mergeFolderPath}`);
+  console.log(`🎯 Targetted merge: ${baseFolderPath}`);
+
+  // let lang = process.argv[2];
+
+  let langs = fs.readdirSync(mergeFolderPath);
+  langs.forEach((lang) => {
+    console.log('........................');
+
+    if (ignorableFolders.includes(lang)) {
+      console.log(`skipped folder: ${lang}`);
+      return true;
+    }
+
+    console.log(`Proccessing folder: ${lang}`);
+
+    let files = fs.readdirSync(`${mergeFolderPath}/${lang}`);
+
+    let [localeFile] = files.filter((file) => !ignorableFiles.includes(file));
+    let keys = JSON.parse(fs.readFileSync(`${mergeFolderPath}/${lang}/${localeFile}`, { encoding: 'utf-8' }));
+
+    // pluralize
+    console.log(`pluralizing file: ${localeFile}`);
+    let pluralizedJSON = pluralize(keys);
+
+    // mapping
+    let code = codeMappings[lang] || lang;
+    if (lang !== code) {
+      console.log(`🔄 mapping found: ${lang} -> ${code}`);
+    }
+
+    // merge
+    let writePath = `${baseFolderPath}/${code}.json`;
+    let fileExists = fs.existsSync(writePath);
+    if (!fileExists) {
+      console.log(`File not found: ${writePath}`);
+      return true;
+    }
+
+    fs.writeFileSync(`${baseFolderPath}/${code}.json`, JSON.stringify(pluralizedJSON, null, 4));
+    console.log(`Updated file: ${writePath}`);
+  });
+};
+
+module.exports = mergeTranslations;
